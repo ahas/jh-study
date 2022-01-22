f1360a74de422c5ded427988455de6688b7ba754
ahas <dev@ahas.io>
2022-01-23 02:16:24 +0900

    완료 4일차

---
+++ day-04/test.js
@@ -0,0 +1,34 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 9; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// function hi(text1, text2) {
+//     console.log("안녕");
+//     console.log(text1);
+//     console.log("반갑" + text2);
+// }
+
+// hi("하세요", "습니다.");
+// hi("하신가요", "핍시다");
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let result = add(add(7, 7), add(7, 7));
+
+for (let i = 0; i < 5; i++) {
+    let 식 = prompt("대화해보세요");
+    switch (식) {
+        case "안녕":
+            alert("나도 안녕");
+            break;
+        case "잘가":
+            alert("너도 잘가");
+            break;
+    }
+}
